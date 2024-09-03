5feb203205bb93de6f8109dbe1c0d548c7f17a34
EHTISHAM-Afzal <khaj3172@gmail.com>
2024-09-03 20:23:41 +0500

    Changes to be committed: 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Dashboard from "@/components/dashboard-03";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-24">
       <Charts />
       <Dashboard />
     </main>
